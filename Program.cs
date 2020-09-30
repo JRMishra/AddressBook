eeb2eacd36e2d5cc7d1e5ed216d5af80d4544e82
JR Mishra <jrmnitdgp@gmail.com>
2020-09-30 16:36:13 +0530

    [JR Mishra] Add. Contact Details added to Address Book

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 
 namespace AddressBook
 {
@@ -7,7 +9,56 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
-            
+            Console.WriteLine("========================");
+
+            Dictionary<string, ContactDetails> addressBook = new Dictionary<string, ContactDetails>();
+
+            Console.WriteLine("Enter\n" +
+                "1 : Add Contact Details To Address Book\n" +
+                "2 : Edit a Contact Detail\n" +
+                "3 : Delete a Contact Detail\n" +
+                "4 : Exit");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1:
+                    AddContactDetails(ref addressBook);
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                default:
+                    break;
+            }
+
+            return;
+        }
+
+        static void AddContactDetails(ref Dictionary<string,ContactDetails> addressBook)
+        {
+            ContactDetails contact = new ContactDetails();
+
+            Console.WriteLine("Enter\n");
+
+            Console.Write("First Name : ");
+            contact.FirstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            contact.LastName = Console.ReadLine();
+            Console.Write("City : ");
+            contact.City = Console.ReadLine();
+            Console.Write("State : ");
+            contact.State = Console.ReadLine();
+            Console.Write("Zip : ");
+            contact.Zip = Console.ReadLine();
+            Console.Write("Phone Number : ");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.Write("Email : ");
+            contact.Email = Console.ReadLine();
+
+            addressBook.Add(contact.FirstName, contact);
+
+            return;
         }
     }
 }
