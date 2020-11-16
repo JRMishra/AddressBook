e4ad3f5b03eb76cb040a8ab57f1832fef2d9db99
JR Mishra <jrmnitdgp@gmail.com>
2020-11-17 01:14:47 +0530

    [JR Mishra]Add. Read & write addressbook details from json server using Rest Sharp

---
+++ AddressBookProgram/DBoperations/JsonServerOperation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AddressBook.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace AddressBook.DBoperations
+{
+    public class JsonServerOperation : IDataSourceOperation
+    {
+        public DictToListMapping ReadFromDataSource()
+        {
+            DictToListMapping dictToList = new DictToListMapping();
+            
+            dictToList.AddressBookName = JsonConvert.DeserializeObject<List<string>>(GetDataList("AddressBookName").Content);
+            dictToList.ContactName = JsonConvert.DeserializeObject<List<string>>(GetDataList("ContactName").Content);
+            dictToList.FirstName = JsonConvert.DeserializeObject<List<string>>(GetDataList("FirstName").Content);
+            dictToList.LastName= JsonConvert.DeserializeObject<List<string>>(GetDataList("LastName").Content);
+            dictToList.City = JsonConvert.DeserializeObject<List<string>>(GetDataList("City").Content);
+            dictToList.State = JsonConvert.DeserializeObject<List<string>>(GetDataList("State").Content);
+            dictToList.ZipCode = JsonConvert.DeserializeObject<List<string>>(GetDataList("ZipCode").Content);
+            dictToList.PhoneNumber = JsonConvert.DeserializeObject<List<string>>(GetDataList("PhoneNumber").Content);
+            dictToList.Email = JsonConvert.DeserializeObject<List<string>>(GetDataList("Email").Content);
+            dictToList.DateAdded = JsonConvert.DeserializeObject<List<DateTime>>(GetDataList("DateAdded").Content);
+
+            return dictToList;
+        }
+
+        public bool WriteToDataSource(DictToListMapping dictToList)
+        {
+            RestClient client = new RestClient("http://localhost:3000");
+            JObject jObjectbody = new JObject();
+            IRestResponse response = new RestResponse();
+            RestRequest request;
+            for (int i=0; i<dictToList.AddressBookName.Count; i++)
+            {
+                jObjectbody.Add("AddressBookName", dictToList.AddressBookName[i]);
+                jObjectbody.Add("ContactName", dictToList.ContactName[i]);
+                jObjectbody.Add("FirstName", dictToList.FirstName[i]);
+                jObjectbody.Add("LastName", dictToList.LastName[i]);
+                jObjectbody.Add("City", dictToList.City[i]);
+                jObjectbody.Add("State", dictToList.State[i]);
+                jObjectbody.Add("Zip", dictToList.ZipCode[i]);
+                jObjectbody.Add("PhoneNumber", dictToList.PhoneNumber[i]);
+                jObjectbody.Add("Email", dictToList.Email[i]);
+                jObjectbody.Add("DateAdded", dictToList.Email[i]);
+
+                request = new RestRequest("/AddressBook",  Method.POST);
+                request.AddParameter("application/Json",jObjectbody, ParameterType.RequestBody);
+
+                response = client.Execute(request);
+                
+            }
+            
+            return (response.StatusCode == System.Net.HttpStatusCode.Created);
+                
+        }
+
+        
+        private IRestResponse GetDataList(string property)
+        {
+            RestClient client = new RestClient("http://localhost:3000");
+            RestRequest request = new RestRequest("/"+property, Method.GET);
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+    }
+}
---
+++ TestAddressBookProgram/TestJsonServerOperation.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddressBook;
+using AddressBook.DBoperations;
+
+namespace TestAddressBookProgram
+{
+    [TestClass]
+    public class TestJsonServerOperation
+    {
+        [TestMethod]
+        public void ReadFromDataSource_ShouldPopulateAddressBookObject_WithDataFromJsonServerFile()
+        {
+            //Arrange 
+            DictToListMapping dictToList = new DictToListMapping();
+            JsonServerOperation jsonServer = new JsonServerOperation();
+            //Act
+            dictToList = jsonServer.ReadFromDataSource();
+            //Assert
+            Assert.AreEqual("General", dictToList.AddressBookName[0]);
+            Assert.AreEqual("Jyoti RanjanMishra", dictToList.ContactName[0]);
+            Assert.AreEqual("Jyoti Ranjan", dictToList.FirstName[0]);
+            Assert.AreEqual("Mishra", dictToList.LastName[0]);
+            Assert.AreEqual("Baripada", dictToList.City[0]);
+            Assert.AreEqual("Odisha", dictToList.State[0]);
+            Assert.AreEqual("970173", dictToList.ZipCode[0]);
+            Assert.AreEqual("jrm@mymail.com", dictToList.Email[0]);
+            Assert.AreEqual(new System.DateTime(2020,11,13), dictToList.DateAdded[0].Date);
+        }
+
+        [TestMethod]
+        public void WriteToDataSource_ShouldWriteToJsonServer_WithSpecifiedData()
+        {
+            //Arrange 
+            DictToListMapping dictToList = new DictToListMapping();
+            dictToList.AddressBookName.Add("Testing");
+            dictToList.ContactName.Add("Testing");
+            dictToList.FirstName.Add("Testing");
+            dictToList.LastName.Add("Testing");
+            dictToList.City.Add("Testing");
+            dictToList.State.Add("Testing");
+            dictToList.ZipCode.Add("Testing");
+            dictToList.PhoneNumber.Add("Testing");
+            dictToList.Email.Add("Testing");
+            dictToList.DateAdded.Add(new System.DateTime(2020,01,01));
+            JsonServerOperation jsonServer = new JsonServerOperation();
+            //Act
+            bool status = jsonServer.WriteToDataSource(dictToList);
+            //Assert
+            Assert.IsTrue(status);
+        }
+
+    }
+}
